50d1a633d449c56ba5a43c9af199449dd4b32cf9
Farkhondeh <f.elena.sh@gmail.com>
2021-09-30 22:37:22 -0700

    UserLoginPage and Mainpage Template and Calling
    UserLoginPage and Mainpage Template and Calling

--- PhotoLibraryUWP/App.xaml.cs
+++ PhotoLibraryUWP/App.xaml.cs
@@ -66,7 +66,9 @@ namespace PhotoLibraryUWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    //Filicity: I changed this line to startting app from UserLoginPage   
+                    // rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(UserLogin), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
