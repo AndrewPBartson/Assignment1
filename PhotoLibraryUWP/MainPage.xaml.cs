44e003a7ba67992f01c93dee259dff216e0bf06f
Farkhondeh <f.elena.sh@gmail.com>
2021-10-03 02:21:00 -0700

    set  Statndard Naming On Pages

--- PhotoLibraryUWP/MainPage.xaml.cs
+++ PhotoLibraryUWP/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,20 +13,23 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using PhotoLibraryUWP.Model;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace PhotoLibraryUWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+   
     public sealed partial class MainPage : Page
     {
+
+        private List<MenuItem> MenuItems;
         public MainPage()
         {
             this.InitializeComponent();
-          //  Frame.Navigate(typeof(UserLogin));
+            //MenuItems = new List<MenuItem>();
+            //MenuItems.Add()
         }
 
         private void PhotoGridView_ItemClick(object sender, ItemClickEventArgs e)
@@ -33,8 +37,34 @@ namespace PhotoLibraryUWP
 
         }
 
-        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        
+
+       
+
+        private void PhotosButton_OnClick(object sender, RoutedEventArgs e)
         {
+            
+
+
+        }
+
+        private void AlbumsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainFeatureListview_ItemClick(object sender, ItemClickEventArgs e)
+        {
+           
+            var ClickedItem = (string)e.ClickedItem;
+
+            if (ClickedItem == "MyPhotos")
+            { 
+                //call show photos in PhotoGrid
+            }
+            else if(ClickedItem == "MyAlbums")
+            { 
+            }
 
         }
     }
