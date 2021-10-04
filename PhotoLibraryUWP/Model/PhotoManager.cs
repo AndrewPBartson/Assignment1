8acb1debc4cd858b26f2eb79efb3877ae6eaef63
swatitayal <31080980+swatitayal@users.noreply.github.com>
2021-10-03 17:35:30 -0700

    Updated classes
    I have updates classes "Photo" and "Photomanager" to be in sync with the data uploaded by Andrew. Included the Assets and classes in the solution as well.

--- PhotoLibraryUWP/Model/PhotoManager.cs
+++ PhotoLibraryUWP/Model/PhotoManager.cs
@@ -31,14 +31,40 @@ namespace PhotoLibraryUWP.Model
         private static List<Photo> getPhotos()
         {
             var photos = new List<Photo>();
-            photos.Add(new Photo("Art", PhotoCategory.Art));
-            photos.Add(new Photo("Watermelon", PhotoCategory.Art));
+            photos.Add(new Photo("bear_cubs", PhotoCategory.Animals));
+            photos.Add(new Photo("chinook", PhotoCategory.Animals));
+            photos.Add(new Photo("elk", PhotoCategory.Animals));
+            photos.Add(new Photo("foxes", PhotoCategory.Animals));
 
-            photos.Add(new Photo("Balloons", PhotoCategory.Celebration));
-            photos.Add(new Photo("Holiday", PhotoCategory.Celebration));
+            photos.Add(new Photo("beach_sunset_people", PhotoCategory.Beaches));
+            photos.Add(new Photo("hotel_beach", PhotoCategory.Beaches));
+            photos.Add(new Photo("oregon", PhotoCategory.Beaches));
+            photos.Add(new Photo("rocky_shore", PhotoCategory.Beaches));
 
-            photos.Add(new Photo("Deer", PhotoCategory.Nature));
-            photos.Add(new Photo("Sunset", PhotoCategory.Nature));
+            photos.Add(new Photo("eagle", PhotoCategory.Birds));
+            photos.Add(new Photo("raven_closeup", PhotoCategory.Birds));
+            photos.Add(new Photo("spotted_owl", PhotoCategory.Birds));
+            
+            photos.Add(new Photo("benicia", PhotoCategory.Bridges));
+            photos.Add(new Photo("golden_gate", PhotoCategory.Bridges));
+            photos.Add(new Photo("mystic_river", PhotoCategory.Bridges));
+            photos.Add(new Photo("richmond_san_rafael", PhotoCategory.Bridges));
+
+            photos.Add(new Photo("butterfly_blue", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_blue_red", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_fat", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_flower", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_green_white_black", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_orange", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_turquoise", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_white_black_orange", PhotoCategory.Butterflies));
+            photos.Add(new Photo("butterfly_yellow_red_white", PhotoCategory.Butterflies));
+
+            photos.Add(new Photo("hongkong_night", PhotoCategory.Cities));
+
+            photos.Add(new Photo("flowers_veggies", PhotoCategory.Flowers));
+
+            photos.Add(new Photo("redwood_sunrays", PhotoCategory.Trees));
 
             return photos;
 
